a567ba27fdf0583b807ef8cff2a864a6208d5711
sumworks <133286611+geslogistics@users.noreply.github.com>
2024-01-16 15:07:35 +0300

    asda

--- ges_logistics_partner/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import res_partner_application
-from . import inherited_models
-from . import res_config_settings
-from . import purchase_team
-from . import crm
