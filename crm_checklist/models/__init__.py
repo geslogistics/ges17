bfb4e16c143d78877c3413770fddad3610c4b5e2
sumworks <133286611+geslogistics@users.noreply.github.com>
2024-01-22 12:41:33 +0300

    del

--- crm_checklist/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import crm_check_list
-from . import crm_check_history
-from . import crm_stage
-from . import crm_lead
--- ges_logistics_request_costing/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import request_costing
-from . import inherited_models
