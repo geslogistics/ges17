9a2847994386c197b956ca012ab008cbb9ace58b
sumworks <133286611+geslogistics@users.noreply.github.com>
2024-02-21 16:52:38 +0300

    de

--- ges_logistics/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import logistics_popup
-from . import logistics_document_configurator
--- ges_logistics_request_costing/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import request_costing
-from . import inherited_models
-from . import ticket
